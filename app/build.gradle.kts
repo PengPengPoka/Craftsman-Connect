f1d30c884742c7ede651b7b944e2a10c5139afcd
Lamz16 <andiku07055@gmail.com>
2023-12-22 12:30:41 +0700

    update library gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,7 +55,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.1")
+    implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.10.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
@@ -66,7 +66,7 @@ dependencies {
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.wear.compose:compose-material:1.2.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -77,33 +77,42 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
+//  This library provides support for integrating Jetpack's Android lifecycle components with Jetpack Compose.
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
 
+    // use viewModel for retain data and business logic outside the lifecycle of Android components
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
 
+
+    //live data in jetpack compose and asynchronous
     implementation("androidx.compose.runtime:runtime:1.5.4")
     implementation("androidx.compose.runtime:runtime-livedata:1.5.4")
     implementation("androidx.compose.runtime:runtime-rxjava2:1.5.4")
 
-    implementation("androidx.navigation:navigation-compose:2.7.5")
+    // navigation for compose
+    implementation("androidx.navigation:navigation-compose:2.7.6")
 
+    // Datastore
     implementation("androidx.datastore:datastore-preferences:1.0.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+
+    // material icons
     implementation("androidx.compose.material:material-icons-extended:1.5.4")
 
+    //Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-
     implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
     implementation("com.squareup.moshi:moshi-kotlin:1.12.0")
 
+    //Load image online with coil
     implementation("io.coil-kt:coil-compose:2.5.0")
 
+    //Layouting with jetpack compose
     implementation("com.google.accompanist:accompanist-insets:0.30.1")
 
+    //kotlin coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-
-    implementation("androidx.compose.runtime:runtime-livedata:1.0.0-beta01")
 }
